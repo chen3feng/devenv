1862ec18d1706532cf30db610c5040f17b41385a
chen3feng <chen3feng@gmail.com>
2023-06-16 11:44:47 +0800

    Add enhanced CD command

--- windows/autorun.bat
+++ windows/autorun.bat
@@ -1,2 +1,17 @@
-@doskey /macrofile=%~dp0doskey.macros
-@doskey git=%~dp0git-wrapper.bat $*
+@echo off
+doskey /macrofile=%~dp0doskey.macros
+doskey git=%~dp0git-wrapper.bat $*
+
+::CD Aliases
+::https://stackoverflow.com/questions/9228950/what-is-the-alternative-for-users-home-directory-on-windows-command-prompt
+::https://stackoverflow.com/questions/48189935/how-can-i-return-to-the-previous-directory-in-windows-command-prompt
+doskey cd=%~dp0cd-wrapper.bat $*
+doskey .=cd
+doskey ..=%~dp0cd-wrapper.bat ..
+doskey ...=%~dp0cd-wrapper.bat ..\..
+doskey ....=%~dp0cd-wrapper.bat ..\..\..
+doskey .....=%~dp0cd-wrapper.bat ..\..\..\..
+doskey ......=%~dp0cd-wrapper.bat ..\..\..\..\..
+doskey .......=%~dp0cd-wrapper.bat ..\..\..\..\..\..
+doskey ........=%~dp0cd-wrapper.bat ..\..\..\..\..\..\..
+doskey .........=%~dp0cd-wrapper.bat ..\..\..\..\..\..\..\..
