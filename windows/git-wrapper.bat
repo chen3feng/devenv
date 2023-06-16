3e18fd24bdd3a0fda40df3859f500c014960ce0b
chen3feng <chen3feng@gmail.com>
2023-06-16 11:03:58 +0800

    Update windows command prompt after git command

--- windows/git-wrapper.bat
+++ windows/git-wrapper.bat
@@ -1,6 +1,10 @@
 :: Show branch name in command prompt. See https://stackoverflow.com/a/52411694
 
 @echo off
+
+git.exe %*
+set /a exitcode=%errorlevel%
+
 set GITBRANCH=
 for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do set GITBRANCH=%%I
 
@@ -10,4 +14,4 @@ if "%GITBRANCH%" == "" (
     prompt $P $C$E[1;7;32;47m%GITBRANCH%$E[0m$F $G 
 )
 
-git.exe %*
+exit /B %exitcode%
