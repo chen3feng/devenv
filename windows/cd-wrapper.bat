35fb8c31622a2ab8afc3bb55a2fa80556a5f1a10
chen3feng <chen3feng@gmail.com>
2023-06-26 19:13:48 +0800

    Fix git branch didn't change issue when cd ~

--- windows/cd-wrapper.bat
+++ windows/cd-wrapper.bat
@@ -1,17 +1,20 @@
 @echo off
-if '%*'=='' cd & exit /b
+
+if '%*'=='' cd && exit /b
+
+set PWD=%cd%
+
 if '%*'=='-' (
-	if '%OLDPWD%' neq '' (
-		cd /d %OLDPWD%
-		call %~dp0git\update-prompt.cmd
-	)
-    set OLDPWD="%cd%"
+	if '%OLDPWD%' == '' exit /b
+	cd /d %OLDPWD%
 ) else if '%*'=='~' (
 	cd /d "%USERPROFILE%"
 ) else (
-    cd /d %*
-    if not errorlevel 1 (
-		set OLDPWD="%cd%"
-		call %~dp0git\update-prompt.cmd
-	)
+	cd /d %*
 )
+
+if errorlevel 1 exit /b %errorlevel%
+if '%cd%' == '%pwd%' exit /b
+
+set OLDPWD=%pwd%
+call %~dp0git\update-prompt.cmd
