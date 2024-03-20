b26044b61e7f8c19b5d97d6a87f5bf3164bc6a43
chen3feng <chen3feng@gmail.com>
2024-03-20 18:59:16 +0800

    Fix bug in git global hook

--- git/install.bat
+++ git/install.bat
@@ -4,10 +4,11 @@ setlocal enabledelayedexpansion
 
 for /f %%i in ('git config --global core.hookspath') do set old=%%i
 set hookspath=%~dp0hooks
+set hookspath=%hookspath:\=/%
 
 if not [%old%] == [] (
 	if not [%hookspath%] == [%old%] (
-		choice /M "global 'core.hooksPath' is already set to '%old%', do you want to overrite it? (y/N) "
+		choice /M "global 'core.hooksPath' is already set to '%old%', do you want to overrite it with '%hookspath%'? (y/N) "
 		if not !errorlevel! == 1 (
 			exit /b 1
 		)
