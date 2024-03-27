167d39116713661d124f4105308d92d70e74d021
chen3feng <chen3feng@gmail.com>
2024-03-27 15:10:09 +0800

    Fix bugs in git finish

--- windows/git/commands.bat
+++ windows/git/commands.bat
@@ -1,36 +1,50 @@
 @echo off
 
 if [%1] == [finish] (
-	shift
 	call :finish %*
 ) else (
 	git.exe %*
 )
 
-goto :eof
+goto :EOF
 
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+:: function finish(args...)
 :finish
-set CURRENT_BRANCH=
-set DEFAULT_BRANCH=
+	setlocal
+	shift
+	if "%1" == "--help" (
+		call :finish_help
+		exit /b
+	)
+	set CURRENT_BRANCH=
+	set DEFAULT_BRANCH=
 
-for /f "tokens=1" %%I in ('git.exe rev-parse --abbrev-ref HEAD 2^> NUL') do set CURRENT_BRANCH=%%I
-for /f "tokens=1" %%I in ('git.exe symbolic-ref refs/remotes/origin/HEAD 2^> NUL') do set DEFAULT_BRANCH=%%I
-set DEFAULT_BRANCH=%DEFAULT_BRANCH:refs/remotes/origin/=%
+	for /f "tokens=1" %%I in ('git.exe rev-parse --abbrev-ref HEAD 2^> NUL') do set CURRENT_BRANCH=%%I
+	for /f "tokens=1" %%I in ('git.exe symbolic-ref -q HEAD --short 2^> NUL') do set DEFAULT_BRANCH=%%I
 
-if "%CURRENT_BRANCH%" == "%DEFAULT_BRANCH%" (
-	echo You are already in the mainline branch.
-	exit /b 1
-)
+	if "%CURRENT_BRANCH%" == "%DEFAULT_BRANCH%" (
+		echo You are already in the mainline branch.
+		exit /b 1
+	)
 
-git.exe diff --name-status --exit-code
-if errorlevel 1 (
-	echo You have uncommitted local changes.
-	exit /b 1
-)
+	git.exe diff --name-status --exit-code
+	if errorlevel 1 (
+		echo You have uncommitted local changes.
+		exit /b 1
+	)
 
-git.exe checkout %DEFAULT_BRANCH%
-:: can be --rebase ?
-shift
-git.exe pull
-goto :eof
+	git.exe checkout %DEFAULT_BRANCH%
+	:: can be --rebase ?
+	shift
+	git.exe pull
+	git.exe branch -D %DEFAULT_BRANCH%
+exit /b
+
+
+:: function finish_help()
+:finish_help
+	echo git finish: Finish your current working branch after it is merged to the mainline remotely.
+	echo It switches to the mainline branch, executes git pull, then deletes the original working branch.
+exit /b
