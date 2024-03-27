1948367a901095019a88b9b9a7c24d44ef1099c6
CHEN Feng <chen3feng@gmail.com>
2024-03-27 15:59:14 +0800

    Fix bugs in git finish (#10)
    * Fix bugs in git finish

--- bin/README.md
+++ bin/README.md
@@ -2,7 +2,11 @@
 
 ## git-finish
 
-git finish 子命令。特性分支开发完成后，回到主分支并立即执行 `git pull`。
+git finish 子命令。
+
+使用场合：
+
+特性分支开发完成并在服务器上合入后。执行该命令回到主分支，并立即执行 `git pull` 更新代码，然后删除原开发分支。
 
 ## ls-go-mods
 
--- windows/git/commands.bat
+++ windows/git/commands.bat
@@ -1,36 +1,53 @@
 @echo off
 
 if [%1] == [finish] (
-	shift
 	call :finish %*
 ) else (
 	git.exe %*
 )
 
-goto :eof
+goto :EOF
 
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+:: function finish(args...)
 :finish
-set CURRENT_BRANCH=
-set DEFAULT_BRANCH=
+	setlocal
+	if "%2" == "--help" (
+		call :finish_help
+		exit /b
+	)
+	set CURRENT_BRANCH=
+	set DEFAULT_BRANCH=
 
-for /f "tokens=1" %%I in ('git.exe rev-parse --abbrev-ref HEAD 2^> NUL') do set CURRENT_BRANCH=%%I
-for /f "tokens=1" %%I in ('git.exe symbolic-ref refs/remotes/origin/HEAD 2^> NUL') do set DEFAULT_BRANCH=%%I
-set DEFAULT_BRANCH=%DEFAULT_BRANCH:refs/remotes/origin/=%
+	for /f "tokens=3" %%I in ('git.exe remote show origin ^| findstr /C:"HEAD branch:" 2^> NUL') do set DEFAULT_BRANCH=%%I
+	for /f "tokens=1" %%I in ('git.exe rev-parse --abbrev-ref HEAD 2^> NUL') do set CURRENT_BRANCH=%%I
 
-if "%CURRENT_BRANCH%" == "%DEFAULT_BRANCH%" (
-	echo You are already in the mainline branch.
-	exit /b 1
-)
+	if "%DEFAULT_BRANCH%" == "" (
+		echo Can't find default branch.
+		exit /b 1
+	)
 
-git.exe diff --name-status --exit-code
-if errorlevel 1 (
-	echo You have uncommitted local changes.
-	exit /b 1
-)
+	if "%CURRENT_BRANCH%" == "%DEFAULT_BRANCH%" (
+		echo You are already in the mainline branch.
+		exit /b 1
+	)
 
-git.exe checkout %DEFAULT_BRANCH%
-:: can be --rebase ?
-shift
-git.exe pull
-goto :eof
+	git.exe diff --name-status --exit-code
+	if errorlevel 1 (
+		echo You have uncommitted local changes.
+		exit /b 1
+	)
+
+	git.exe checkout %DEFAULT_BRANCH%
+	:: can be --rebase ?
+	git.exe pull
+	git.exe branch -D %CURRENT_BRANCH%
+exit /b
+
+
+:: function finish_help()
+:finish_help
+	echo git finish: Finish your current working branch after it is merged to the mainline remotely.
+	echo It switches to the mainline branch, executes git pull, then deletes the original working branch.
+exit /b
