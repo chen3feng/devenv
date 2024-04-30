3f088bfffe731959967334f5e9e97058c3e1d1e5
chen3feng <chen3feng@gmail.com>
2024-04-30 17:09:28 +0800

    Speedup xargs.bat

--- windows/bin/xargs.bat
+++ windows/bin/xargs.bat
@@ -5,14 +5,14 @@
 
 setlocal enabledelayedexpansion
 
-set args=
+set cmd=
 set file='more'
 
 :: read from file
 if "%1" == "-a" (
     if "%2" == "" (
         echo Correct Usage: %0 -a Input.txt command
-        goto end
+        goto :EOF
     )
     set file=%2
     shift
@@ -21,16 +21,62 @@ if "%1" == "-a" (
 )
 
 :: read from stdin
-set args=%1
+set cmd=%1
 shift
 
 :start
     if [%1] == [] goto start1
-    set args=%args% %1
+    set cmd=%cmd% %1
     shift
     goto start
 
 :start1
-    for /F "tokens=*" %%a in (!file!) do call %args% %%a
+    set args=
+    for /F "tokens=*" %%a in (!file!) do (
+        if [!args!] == [] (
+            set "new_args=%%a"
+        ) else (
+            set "new_args=!args! %%a"
+        )
+        call :strlen len "!new_args!"
+        :: https://learn.microsoft.com/en-us/troubleshoot/windows-client/shell-experience/command-line-string-limitation
+        if !len! gtr 8000 (
+            if not [!args!] == [] (
+                call %cmd% !args!
+            )
+            set "args=%%a"
+        ) else (
+            set "args=!new_args!"
+        )
+    )
+    if not [!args!] == [] (
+        call %cmd% !args!
+    )
 
-:end
+
+goto :EOF
+
+
+::https://stackoverflow.com/questions/5837418/how-do-you-get-the-string-length-in-a-batch-file
+REM ********* function *****************************
+:strlen <resultVar> <stringVar>
+(   
+    setlocal EnableDelayedExpansion
+    (set^ tmp=!%~2!)
+    if defined tmp (
+        set "len=1"
+        for %%P in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
+            if "!tmp:~%%P,1!" NEQ "" ( 
+                set /a "len+=%%P"
+                set "tmp=!tmp:~%%P!"
+            )
+        )
+    ) else (
+        set len=0
+    )
+)
+( 
+    endlocal
+    set "%~1=%len%"
+    exit /b
+)
